30fa1ce9840c398281939bae8d5935441cd60314
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-01-15 12:30:41 +0100

    Creating flutter project, adding contents i. e. pictures

---
+++ small_gallery_application/README.md
@@ -0,0 +1,3 @@
+# small_gallery_application
+
+A new Flutter project.
---
+++ small_gallery_application/android/app/src/main/kotlin/com/example/small_gallery_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.small_gallery_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
