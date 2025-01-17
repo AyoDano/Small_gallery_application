c47b61ebc45269b517dac0a0f21cc4548991266a
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-01-17 13:04:53 +0100

    create gallery screen, adding AppBar, bottom navigation, customcards and connecting with navigator widget, DetailPage with appbar, title, date,

---
+++ small_gallery_application/lib/01_main.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GalleryApp(),
+    );
+  }
+}
+
+class GalleryApp extends StatefulWidget {
+  const GalleryApp({super.key});
+
+  @override
+  _GalleryAppState createState() => _GalleryAppState();
+}
+
+class _GalleryAppState extends State<GalleryApp> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const GalleryScreen(),
+    const Center(child: Text("About Me")),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Gallery",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_camera),
+            label: "Gallery",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "About Me",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GalleryScreen extends StatelessWidget {
+  const GalleryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 1,
+        children: [
+          CustomCard(
+            title: "Colourful Basketball",
+            imagePath: "assets/images/basketball_1.jpeg",
+            description:
+                "A colourful outdoor basketball court. In the centre is a basketball hoop, its white net swaying in the gentle breeze. The colours of the court glow under the warm evening sun. Many years ago, during a summer evening, friends met here for an unforgettable game in which the winners cheered the sunset.",
+            date: "03.06.2023",
+            context: context,
+          ),
+          CustomCard(
+            title: "Watch out for bends",
+            imagePath: "assets/images/bike.jpeg",
+            description:
+                "A large group of racing cyclists lean into a sharp bend in synchronisation as they race along the track at an impressive speed. The riders' colourful jerseys glow in the sunshine and their concentration is palpable. A few years ago, during an exciting competition on a sunny spring day, this bend marked the turning point of the race and became the scene of a thrilling duel for the top spot.",
+            date: "18.06.2022",
+            context: context,
+          ),
+          CustomCard(
+            title: "Watch out for bends",
+            imagePath: "assets/images/bike.jpeg",
+            description:
+                "A large group of racing cyclists lean into a sharp bend in synchronisation as they race along the track at an impressive speed. The riders' colourful jerseys glow in the sunshine and their concentration is palpable. A few years ago, during an exciting competition on a sunny spring day, this bend marked the turning point of the race and became the scene of a thrilling duel for the top spot.",
+            date: "18.06.2022",
+            context: context,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final String description;
+  final String date;
+  final BuildContext context; // Der Kontext für die Navigation
+
+  const CustomCard({
+    super.key,
+    required this.title,
+    required this.imagePath,
+    required this.description,
+    required this.date,
+    required this.context,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Wenn die Karte getippt wird, öffne die Detailseite und übergebe die Daten
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(
+              title: title,
+              imagePath: imagePath,
+              description: description,
+              date: date,
+            ),
+          ),
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(8)),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final String description;
+  final String date;
+
+  const DetailPage({
+    super.key,
+    required this.title,
+    required this.imagePath,
+    required this.description,
+    required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title), // Titel der Detailseite
+        leading: IconButton(
+          icon: const Icon(
+              Icons.chevron_left), // Chevron-Icon für den Zurück-Button
+          onPressed: () {
+            Navigator.pop(
+                context); // Führt dazu, dass die Detailseite geschlossen wird
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 300,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                date, // Das übergebene Datum wird hier angezeigt
+                style: const TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                description, // Die individuelle Beschreibung wird hier angezeigt
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- small_gallery_application/lib/gallery_data.dart
+++ small_gallery_application/lib/gallery_data.dart
@@ -1,51 +1,52 @@
-List<GalleryItem> galleryData = [
+/* List<GalleryItem> galleryData = [
   GalleryItem(
     imageTitle: "Colourful basketball",
     imageDate: '03.06.2023',
     imageDescription:
         "A colourful outdoor basketball court. In the centre is a basketball hoop, its white net swaying in the gentle breeze. The colours of the court glow under the warm evening sun. Many years ago, during a summer evening, friends met here for an unforgettable game in which the winners cheered the sunset.",
-    imagePath: 'assets/images/basketball_1.jpg',
+    imagePath: 'assets/images/basketball_1.jpeg',
   ),
   GalleryItem(
     imageTitle: 'Pure adrenaline!',
     imageDate: '14.07.2020',
     imageDescription:
         "A skier in full kit flies boldly over a snow-covered hill, while the clear blue sky shines in the background. The sun shines brightly, illuminating the sparkling snowflakes and casting long shadows on the piste. On a perfect winter's day a few years ago, when the snow was fresh and the conditions ideal, this jump led to an unforgettable moment full of adrenaline and joy",
-    imagePath: 'assets/images/ski.jpg',
+    imagePath: 'assets/images/ski.jpeg',
   ),
   GalleryItem(
     imageTitle: 'Watch out for bends',
     imageDate: '18.06.2022',
     imageDescription:
         "A large group of racing cyclists lean into a sharp bend in synchronisation as they race along the track at an impressive speed. The riders' colourful jerseys glow in the sunshine and their concentration is palpable. A few years ago, during an exciting competition on a sunny spring day, this bend marked the turning point of the race and became the scene of a thrilling duel for the top spot",
-    imagePath: 'assets/images/bike.jpg',
+    imagePath: 'assets/images/bike.jpeg',
   ),
   GalleryItem(
     imageTitle: 'Runners High',
     imageDate: '22.09.2023',
     imageDescription:
         "Our eight runners move in an impressive triangular formation across the tracks of the sunny sports field at Mauerpark. All the athletes are synchronised, their steps in perfect unison as they feel the red tartan track beneath their feet. On a chilly autumn morning, during an intense training session, this formation was chosen to perfect teamwork and precision, and it was a moment that reflected the determination and cohesion of the group",
-    imagePath: 'assets/images/running.jpg',
+    imagePath: 'assets/images/running.jpeg',
   ),
   GalleryItem(
     imageTitle: 'Swish',
     imageDate: '11.08.2018',
     imageDescription:
         "A photo taken from below shows a basketball hoop with a bright blue sky in the background. The sun is shining brightly and the white net of the basket stands out against the blue. On a clear summer afternoon, many years ago, a legendary game was played here, with the last basket shining in the golden light of the evening",
-    imagePath: 'assets/images/basketball_2.jpg',
+    imagePath: 'assets/images/basketball_2.jpeg',
   ),
   GalleryItem(
     imageTitle: 'Ball Game',
     imageDate: '29.11.2023',
     imageDescription:
         "The close-up shows a yellow-green tennis ball lying on the shiny floor of the sports hall. The texture of the ball is clearly visible, while the subdued light of the hall creates a calm atmosphere. On a rainy afternoon, an exciting match took place here, in which this ball played an important role at the decisive moment of the game",
-    imagePath: 'assets/images/tennis_1.jpg',
+    imagePath: 'assets/images/tennis_1.jpeg',
   ),
   GalleryItem(
     imageTitle: 'Victory in nature',
     imageDate: '02.05.2022',
     imageDescription:
         "An impressive drone shot shows a tennis court in an idyllic location surrounded by greenery. The bright green court contrasts beautifully with the surrounding trees and meadows. On a sunny summer's day a few years ago, a tournament was held here, with players and spectators alike enjoying the breathtaking natural scenery",
-    imagePath: 'assets/images/tennis_2.jpg',
+    imagePath: 'assets/images/tennis_2.jpeg',
   ),
 ];
+*/
